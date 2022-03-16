cefb868473f9cfc22dd6263be6c4b11e69179294
matcha <matchadcaf@protonmail.com>
2022-03-16 08:52:40 +0400

    feat: add drip-by-dcaf logo

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -1,8 +1,26 @@
+import { Image } from '@chakra-ui/react';
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 import { FC } from 'react';
 import styled from 'styled-components';
+import Logo from '../assets/logo.svg';
 
 const StyledContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const StyledLeftContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+`;
+
+const StyledRightContainer = styled.div`
   width: 100%;
   display: flex;
   flex-direction: row;
@@ -10,10 +28,19 @@ const StyledContainer = styled.div`
   justify-content: flex-end;
 `;
 
+const StyledLogo = styled(Image)`
+  width: 120px;
+`;
+
 export const Header: FC = () => {
   return (
     <StyledContainer>
-      <WalletMultiButton />
+      <StyledLeftContainer>
+        <StyledLogo src={Logo} />
+      </StyledLeftContainer>
+      <StyledRightContainer>
+        <WalletMultiButton />
+      </StyledRightContainer>
     </StyledContainer>
   );
 };
