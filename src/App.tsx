abb9ba1e949c0684e760096e5c381dd7d8fb599d
matcha <matchadcaf@protonmail.com>
2022-03-16 08:17:38 +0400

    chore: move wallet context to its own file

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,7 @@
 import { ChakraProvider } from '@chakra-ui/react';
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
-import {
-  LedgerWalletAdapter,
-  PhantomWalletAdapter,
-  SlopeWalletAdapter,
-  SolflareWalletAdapter,
-  SolletExtensionWalletAdapter,
-  SolletWalletAdapter,
-  TorusWalletAdapter
-} from '@solana/wallet-adapter-wallets';
-import { clusterApiUrl } from '@solana/web3.js';
-import { FC, ReactNode, useMemo } from 'react';
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui';
+import { FC } from 'react';
+import { WalletContext } from './contexts';
 import { theme } from './theme';
 
 require('./App.css');
@@ -21,45 +10,13 @@ require('@solana/wallet-adapter-react-ui/styles.css');
 const App: FC = () => {
   return (
     <ChakraProvider theme={theme}>
-      <Context>
+      <WalletContext>
         <Content />
-      </Context>
+      </WalletContext>
     </ChakraProvider>
   );
 };
 export default App;
-
-const Context: FC<{ children: ReactNode }> = ({ children }) => {
-  // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
-  const network = WalletAdapterNetwork.Devnet;
-
-  // You can also provide a custom RPC endpoint.
-  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
-
-  // @solana/wallet-adapter-wallets includes all the adapters but supports tree shaking and lazy loading --
-  // Only the wallets you configure here will be compiled into your application, and only the dependencies
-  // of wallets that your users connect to will be loaded.
-  const wallets = useMemo(
-    () => [
-      new PhantomWalletAdapter(),
-      new SlopeWalletAdapter(),
-      new SolflareWalletAdapter({ network }),
-      new TorusWalletAdapter(),
-      new LedgerWalletAdapter(),
-      new SolletWalletAdapter({ network }),
-      new SolletExtensionWalletAdapter({ network })
-    ],
-    [network]
-  );
-
-  return (
-    <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={wallets} autoConnect>
-        <WalletModalProvider>{children}</WalletModalProvider>
-      </WalletProvider>
-    </ConnectionProvider>
-  );
-};
 
 const Content: FC = () => {
   return (
