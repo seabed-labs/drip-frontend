3ccea97ea83da3c1a6829816ef31d3e11733c17d
Cappuccino <cappdcaf@protonmail.com>
2022-03-17 02:56:26 -0400

    grid draft

--- src/pages/Positions.tsx
+++ src/pages/Positions.tsx
@@ -1,6 +1,28 @@
-import { Center } from '@chakra-ui/react';
+import { Grid, GridItem } from '@chakra-ui/react';
 import { FC } from 'react';
+import { PositionCard } from '../component/PositionCard';
 
 export const Positions: FC = () => {
-  return <Center>Positions</Center>;
+  return (
+    <Grid templateColumns="repeat(3, 1fr)" gap={6}>
+      <GridItem mt="80px">
+        <PositionCard></PositionCard>
+      </GridItem>
+      <GridItem mt="80px">
+        <PositionCard></PositionCard>
+      </GridItem>
+      <GridItem mt="80px">
+        <PositionCard></PositionCard>
+      </GridItem>
+      <GridItem mt="80px">
+        <PositionCard></PositionCard>
+      </GridItem>
+      <GridItem mt="80px">
+        <PositionCard></PositionCard>
+      </GridItem>
+      <GridItem mt="80px">
+        <PositionCard></PositionCard>
+      </GridItem>
+    </Grid>
+  );
 };
