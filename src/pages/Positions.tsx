a0997c38e52cf1b83a80299eda4584ee7d564914
matcha <matchadcaf@protonmail.com>
2022-03-17 11:07:26 +0400

    Dynamic list of positions

--- src/pages/Positions.tsx
+++ src/pages/Positions.tsx
@@ -2,27 +2,66 @@ import { Grid, GridItem } from '@chakra-ui/react';
 import { FC } from 'react';
 import { PositionCard } from '../component/PositionCard';
 
+interface Position {
+  tokenA: string;
+  tokenB: string;
+}
+
+function usePositions(): Position[] {
+  return [
+    {
+      tokenA: 'USDC',
+      tokenB: 'SOL'
+    },
+    {
+      tokenA: 'USDC',
+      tokenB: 'BTC'
+    },
+    {
+      tokenA: 'USDC',
+      tokenB: 'ETH'
+    },
+    {
+      tokenA: 'USDC',
+      tokenB: 'ETH'
+    },
+    {
+      tokenA: 'USDC',
+      tokenB: 'SOL'
+    },
+    {
+      tokenA: 'USDC',
+      tokenB: 'BTC'
+    },
+    {
+      tokenA: 'USDC',
+      tokenB: 'ETH'
+    },
+    {
+      tokenA: 'USDC',
+      tokenB: 'SOL'
+    },
+    {
+      tokenA: 'USDC',
+      tokenB: 'BTC'
+    },
+    {
+      tokenA: 'USDC',
+      tokenB: 'ETH'
+    }
+  ];
+}
+
 export const Positions: FC = () => {
+  const positions = usePositions();
+
   return (
-    <Grid templateColumns="repeat(3, 1fr)" gap={6}>
-      <GridItem mt="80px">
-        <PositionCard></PositionCard>
-      </GridItem>
-      <GridItem mt="80px">
-        <PositionCard></PositionCard>
-      </GridItem>
-      <GridItem mt="80px">
-        <PositionCard></PositionCard>
-      </GridItem>
-      <GridItem mt="80px">
-        <PositionCard></PositionCard>
-      </GridItem>
-      <GridItem mt="80px">
-        <PositionCard></PositionCard>
-      </GridItem>
-      <GridItem mt="80px">
-        <PositionCard></PositionCard>
-      </GridItem>
+    <Grid templateColumns="repeat(3, 1fr)" gap="80px">
+      {positions.map((position) => (
+        <GridItem mt="80px">
+          <PositionCard></PositionCard>
+        </GridItem>
+      ))}
     </Grid>
   );
 };
