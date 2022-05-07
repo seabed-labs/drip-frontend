4157584155baad6e6d2ef90646aa8bad314cce8f
matcha <matchadcaf@protonmail.com>
2022-05-07 14:43:13 +0400

    chore: add test USDC and ETH to mint remap object

--- src/hooks/MintRemap.ts
+++ src/hooks/MintRemap.ts
@@ -5,9 +5,20 @@ import { NetworkAddress } from '../models/NetworkAddress';
 const MINT_REMAP: Record<Network, Partial<Record<string, NetworkAddress>>> = {
   [Network.Mainnet]: {},
   [Network.Devnet]: {
+    // wSOL
     BfqATYbPZJFdEdYWkEbFRBnhv1LB6wtLn299HjMmE4uq: NetworkAddress.from(
       Network.Mainnet,
       'So11111111111111111111111111111111111111112'
+    ),
+    // USDC
+    ASuqwxvC4FXxJGT9XqZMXbCKDQBaRTApEhN2oN3VL3A8: NetworkAddress.from(
+      Network.Mainnet,
+      'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v'
+    ),
+    // ETH
+    '54uPbRrVwYsvZZMmsCXjLCtshgTquBWDZTNTJNbEDZ4H': NetworkAddress.from(
+      Network.Mainnet,
+      '7vfCXTUXx5WJV5JADk17DUJ4ksgau7utNKj4b963voxs'
     )
   }
 };
