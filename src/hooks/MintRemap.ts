5df0f0e02ac62ff8da3775f1eb166b6890e8d8d1
Mocha Dcaf <dcafmocha@protonmail.com>
2022-07-17 01:13:11 -0400

    chore: update config

--- src/hooks/MintRemap.ts
+++ src/hooks/MintRemap.ts
@@ -6,14 +6,24 @@ const MINT_REMAP: Record<Network, Partial<Record<string, NetworkAddress>>> = {
   [Network.Mainnet]: {},
   [Network.Devnet]: {
     // wSOL
-    GAstZSga54WrsQdDZ9pgch6Xe9Lyxaiyx4sY23nKyEZ4: NetworkAddress.from(
+    '8LXJFDvW9nrp4MXb4rSW7eSvxAhdzmdiUpNHiDdzJaNP': NetworkAddress.from(
       Network.Mainnet,
       'So11111111111111111111111111111111111111112'
     ),
     // USDC
-    E3R1FopVaFpPAXojjdhJvWfKyaPuGHrx2GY7RCvHn7Dy: NetworkAddress.from(
+    BzTNAVYYHn9aSfUKeNQ2Fv635MsY6WnHMVRMZgb4hbc6: NetworkAddress.from(
       Network.Mainnet,
       'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v'
+    ),
+    // ETH
+    DqakM9iwYs425rToh9LCXzfzc1Xh3A3nrz36QkSJTfNd: NetworkAddress.from(
+      Network.Mainnet,
+      '7vfCXTUXx5WJV5JADk17DUJ4ksgau7utNKj4b963voxs'
+    ),
+    // ETH
+    EMtrTfsR7FvvJFg8J6g9y332L4PGzo3qDSztQbvbswpg: NetworkAddress.from(
+      Network.Mainnet,
+      '9n4nbM75f5Ui33ZbPYXn59EwSgE8CGsHtAeTH5YFeJ9E'
     )
   }
 };
