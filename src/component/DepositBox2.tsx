25d1c6a5ed613ac48346fb3dd1d6c130c8d99da7
Mocha Dcaf <mocha@dcaf.so>
2022-08-14 12:35:30 -0400

    fix: don't reset granularity

--- src/component/DepositBox2.tsx
+++ src/component/DepositBox2.tsx
@@ -161,7 +161,6 @@ export function DepositBox() {
     });
     refreshContext.forceRefresh();
     setDripUntil(undefined);
-    setGranularity(undefined);
     setDepositAmountStr(undefined);
     return txInfo;
   }, [
