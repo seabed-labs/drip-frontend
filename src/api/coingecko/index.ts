8875cb97c25ef09812583b76488d8d96bf011864
Latte <latte@dcaf.so>
2022-09-06 15:49:15 -0500

    chore: remove coingecko-api-v3 third party client

--- src/api/coingecko/index.ts
+++ src/api/coingecko/index.ts
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import { CoinGeckoClient } from 'coingecko-api-v3';
 
 const BASE_URL = 'https://api.coingecko.com';
 const BLOCKCHAIN_NETWORK = 'solana';
@@ -8,27 +7,9 @@ const USD_CURRENCY = 'usd';
 export type ValidDenominations = 'usd' | 'btc' | 'eth' | 'sol';
 
 export class CoingeckoAPI {
-  cgClient: CoinGeckoClient;
   headers = {
     Accept: 'application/json'
   };
-
-  constructor() {
-    this.cgClient = new CoinGeckoClient({
-      timeout: 10000,
-      autoRetry: true
-    });
-  }
-
-  // Uses the third party sdk client to get price by tokenId
-  // @param tokenId = 'solana' OR 'solana,ethereum'
-  async getPriceForTokenById(tokenId: string, denomination: ValidDenominations): Promise<number> {
-    const response = await this.cgClient.simplePrice({
-      ids: tokenId,
-      vs_currencies: denomination
-    });
-    return response[tokenId][denomination];
-  }
 
   async getUSDPriceForTokenByMint(mintAddress: string): Promise<number | undefined> {
     // The API can support comma seperated mint addresses, but to keep the usage simple only allowing
