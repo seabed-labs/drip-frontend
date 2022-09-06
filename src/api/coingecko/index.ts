a0b4019d311249d1078498da876d64f4202df18f
Latte <latte@dcaf.so>
2022-09-06 16:09:21 -0500

    Hardcode value for USDC mint on mainnet

--- src/api/coingecko/index.ts
+++ src/api/coingecko/index.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const BASE_URL = 'https://api.coingecko.com';
+const USD_MINT_SOLANA_MAINNET = 'EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v';
 const BLOCKCHAIN_NETWORK = 'solana';
 const USD_CURRENCY = 'usd';
 
@@ -14,6 +15,12 @@ export class CoingeckoAPI {
   async getUSDPriceForTokenByMint(mintAddress: string): Promise<number | undefined> {
     // The API can support comma seperated mint addresses, but to keep the usage simple only allowing
     // a single mint for this function
+
+    // For some reason, coinGecko doesn't return a value for USD mint
+    if (mintAddress === USD_MINT_SOLANA_MAINNET) {
+      return 1;
+    }
+
     if (mintAddress.split(',').length != 1) {
       throw new Error('Please provide a single mint address.');
     }
