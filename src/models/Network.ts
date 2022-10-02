c57e001ba532083cad9861d8e83ca06e7c6a9978
Mocha Dcaf <mocha@dcaf.so>
2022-10-01 21:51:28 -0400

    chore: upgrade to quicknode pro

--- src/models/Network.ts
+++ src/models/Network.ts
@@ -26,9 +26,9 @@ export function isSupportedENV(env: ENV): boolean {
 export function getClusterApiUrl(network: Network): string {
   switch (network) {
     case Network.Mainnet:
-      return 'https://dimensional-young-cloud.solana-mainnet.discover.quiknode.pro/a5a0fb3cfa38ab740ed634239fd502a99dbf028d';
+      return 'https://dimensional-young-cloud.solana-mainnet.quiknode.pro/a5a0fb3cfa38ab740ed634239fd502a99dbf028d';
     case Network.Devnet:
-      return 'https://api.devnet.solana.com';
+      return 'https://fabled-bitter-tent.solana-devnet.quiknode.pro/ea2807069cec3658c0e16618bea5a5c9b85e0dd7';
     default:
       return 'http://127.0.0.1:8899';
   }
