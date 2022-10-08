8f73e7ca19863fab8a6e559b32d63b6cb415c28e
Mocha Dcaf <mocha@dcaf.so>
2022-10-08 12:00:16 -0400

    chore: remove spl-token-registry

--- src/models/Network.ts
+++ src/models/Network.ts
@@ -1,27 +1,5 @@
 import { Network } from '@dcaf-labs/drip-sdk/dist/models';
-import { ENV } from '@solana/spl-token-registry';
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-
-export function toENV(network: Network): ENV {
-  switch (network) {
-    case Network.Mainnet:
-      return ENV.MainnetBeta;
-    case Network.Devnet:
-      return ENV.Devnet;
-    default:
-      return ENV.Devnet;
-  }
-}
-
-export function isSupportedENV(env: ENV): boolean {
-  switch (env) {
-    case ENV.MainnetBeta:
-    case ENV.Devnet:
-      return true;
-    default:
-      return false;
-  }
-}
 
 export function getClusterApiUrl(network: Network): string {
   switch (network) {
