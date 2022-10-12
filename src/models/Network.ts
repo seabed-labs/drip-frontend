a6e25745d0b293a3ad55af924aa036f3fe6aa050
matcha <matchadcaf@protonmail.com>
2022-10-12 15:12:54 -0700

    Update RPC URL

--- src/models/Network.ts
+++ src/models/Network.ts
@@ -4,8 +4,8 @@ import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 export function getClusterApiUrl(network: Network): string {
   switch (network) {
     case Network.Mainnet:
-      return 'https://api.mainnet-beta.solana.com';
-    // return 'https://dimensional-young-cloud.solana-mainnet.quiknode.pro/a5a0fb3cfa38ab740ed634239fd502a99dbf028d';
+      return 'https://palpable-warmhearted-hexagon.solana-mainnet.discover.quiknode.pro/5793cf44e6e16325347e62d571454890f16e0388';
+    // return 'https://api.mainnet-beta.solana.com';
     case Network.Devnet:
       return 'https://api.devnet.solana.com';
     // return 'https://fabled-bitter-tent.solana-devnet.quiknode.pro/ea2807069cec3658c0e16618bea5a5c9b85e0dd7';
