62b5b92524595cfee87ba8df33768f84ceb10871
Mocha Dcaf <mocha@dcaf.so>
2022-10-12 08:12:38 -0400

    fix: revert to solana rpc api

--- src/models/Network.ts
+++ src/models/Network.ts
@@ -4,9 +4,11 @@ import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 export function getClusterApiUrl(network: Network): string {
   switch (network) {
     case Network.Mainnet:
-      return 'https://dimensional-young-cloud.solana-mainnet.quiknode.pro/a5a0fb3cfa38ab740ed634239fd502a99dbf028d';
+      return 'https://api.mainnet-beta.solana.com';
+    // return 'https://dimensional-young-cloud.solana-mainnet.quiknode.pro/a5a0fb3cfa38ab740ed634239fd502a99dbf028d';
     case Network.Devnet:
-      return 'https://fabled-bitter-tent.solana-devnet.quiknode.pro/ea2807069cec3658c0e16618bea5a5c9b85e0dd7';
+      return 'https://api.devnet.solana.com';
+    // return 'https://fabled-bitter-tent.solana-devnet.quiknode.pro/ea2807069cec3658c0e16618bea5a5c9b85e0dd7';
     default:
       return 'http://127.0.0.1:8899';
   }
