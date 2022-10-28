386ba4b510768e1e4d314e7c24cd609237b6acc7
Mocha <mocha@dcaf.so>
2022-10-28 16:10:36 -0400

    `[chore]`: upgrade deps and support backpack wallet `[DRIP-469]` (#70)

--- config-overrides.js
+++ config-overrides.js
@@ -20,6 +20,9 @@ module.exports = function (config, env) {
     plugins: [
       ...config.plugins,
       new ProvidePlugin({
+        Buffer: ['buffer', 'Buffer']
+      }),
+      new ProvidePlugin({
         process: 'process/browser'
       })
     ],
