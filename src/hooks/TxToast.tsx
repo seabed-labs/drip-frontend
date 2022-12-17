15c17e03d450ceb4f7dd8ff6b6598fecc6dfa239
Dcaf Cortado <dcafcortado@protonmail.com>
2022-12-17 22:58:10 +0400

    functionality of explorers work

--- src/hooks/TxToast.tsx
+++ src/hooks/TxToast.tsx
@@ -1,10 +1,40 @@
 import { Box, Link, useToast } from '@chakra-ui/react';
 import { BroadcastTransactionWithMetadata } from '@dcaf-labs/drip-sdk/dist/types';
 import { useMemo } from 'react';
+import { Network } from '@dcaf-labs/drip-sdk';
+import { useNetwork } from '../contexts/NetworkContext';
 
 interface TxToast {
   success(txInfo: BroadcastTransactionWithMetadata<unknown>): void;
   failure(error: Error): void;
+}
+
+function getSolscanUrl(
+  network: Network,
+  txInfo: BroadcastTransactionWithMetadata<unknown>
+): string {
+  switch (network) {
+    case Network.Mainnet:
+      return `https://solscan.io/tx/${txInfo.id}`;
+    case Network.Devnet:
+      return `https://solscan.io/tx/${txInfo.id}?cluster=devnet`;
+    case Network.Localnet:
+      return ``;
+  }
+}
+
+function getExplorerUrl(
+  network: Network,
+  txInfo: BroadcastTransactionWithMetadata<unknown>
+): string {
+  switch (network) {
+    case Network.Mainnet:
+      return `https://explorer.solana.com/tx/${txInfo.id}`;
+    case Network.Devnet:
+      return `https://explorer.solana.com/tx/${txInfo.id}?cluster=devnet`;
+    case Network.Localnet:
+      return ``;
+  }
 }
 
 function mapErrorMesage(message: string): string {
@@ -19,7 +49,7 @@ function mapErrorMesage(message: string): string {
 }
 export function useTxToast(): TxToast {
   const toast = useToast();
-
+  const network = useNetwork();
   return useMemo(
     () => ({
       success(txInfo: BroadcastTransactionWithMetadata<unknown>) {
@@ -27,9 +57,19 @@ export function useTxToast(): TxToast {
           title: 'Transaction successful',
           description: (
             <Box>
-              <Link href={txInfo.explorer} isExternal textDecoration="underline">
-                Explorer
+              <Link href={getExplorerUrl(network, txInfo)} isExternal textDecoration="overline">
+                Solana Explorer
               </Link>
+              <Link href={getSolscanUrl(network, txInfo)} isExternal textDecoration="underline">
+                Solscan
+              </Link>
+
+              {/* <Link href={`${getExplorerUrl}`} isExternal textDecoration="overline">
+                Solana Explorer
+              </Link>
+              <Link href={`${getSolscanUrl}`} isExternal textDecoration="underline">
+                Solscan
+              </Link> */}
             </Box>
           ),
           status: 'success',
